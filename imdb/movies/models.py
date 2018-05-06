6a36a905ac1bba8738e58a2a5046d4552856531b
AdrianKuriata <adriandj83@gmail.com>
2018-05-06 12:48:44 +0200

    Changed property names and fields args'

--- imdb/movies/models.py
+++ imdb/movies/models.py
@@ -1,30 +1,31 @@
 from django.db import models
+from django.utils.text import Truncator
+from django.utils.dateparse import parse_duration
 
 class Movie(models.Model):
     genres = models.ManyToManyField('movies.genre', related_name='movies', blank=True)
-    title = models.CharField(max_length=550)
-    slug = models.SlugField(max_length=550, unique=True)
-    excerpt = models.CharField(max_length=300)
-    cover = models.ImageField(upload_to='covers', default=None)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=120, unique=True)
+    excerpt = models.CharField(max_length=120)
+    cover = models.ImageField(upload_to='covers', null=True)
     description = models.TextField()
     release_date = models.DateField()
-    duration_time = models.PositiveSmallIntegerField()
+    duration_time = models.DurationField(null=True)
 
     def __str__(self):
         return self.title
 
     @property
-    def get_human_duration_time(self):
-        return '{:02d}h {:02d}m'.format(*divmod(self.duration_time, 60))
+    def converted_duration_time(self):
+        return '{:02d}h {:02d}m'.format(*divmod(self.duration_time.seconds, 60))
 
     @property
-    def get_cut_excerpt(self):
-        cutted = self.excerpt[:100]
-        return cutted + ' ...'
+    def truncated_excerpt(self):
+        return Truncator(self.excerpt).chars(100)
 
 class Genre(models.Model):
-    name = models.CharField(max_length=300)
-    slug = models.SlugField(max_length=300)
+    name = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=120)
 
     def __str__(self):
         return self.name
